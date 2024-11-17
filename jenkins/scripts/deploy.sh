bb2126605ff31bf4963c997c0b8a5d53f57a8b0f
lopmade <aldenwong@live.com>
2024-11-17 22:49:53 +0800

    a

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
-# docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\alden\\Desktop\\ssdtest\\lab7\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+# docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\alden\\Desktop\\ssdtest\\lab7\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
